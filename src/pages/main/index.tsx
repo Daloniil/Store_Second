5fd1e6d295a9ea3290e25e9a0ee7b724a3fd8968
Daloniil <62095873+Daloniil@users.noreply.github.com>
2023-05-08 01:11:12 +0300

    Assets

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,84 +1,3 @@
-// import {useEffect, useState} from "react";
-// import {ADMIN_UID} from "@/services/localKey";
-// import Router from "next/router";
-// import {Auth} from "@/Interfaces/ProvidersInterface";
-// import {useAuth} from "@/hooks/useAuth";
-// import {useCartContext} from "@/hooks/useCartContext";
-// import {useCart} from "@/hooks/useCart";
-// import {Box} from "@mui/material";
-// import Image from "next/image";
-// import toy from "../../assets/1.jpg"
-// import short_woman from "../../assets/2.jpeg"
-// import short_man from "../../assets/3.jpeg"
-// import necklaces from "../../assets/4.jpeg"
-// import bag from "../../assets/5.jpeg"
-// import lamp from "../../assets/6.jpeg"
-// import {redirectTo} from "@/utils/redirect";
-//
-//
-// const MainPage = () => {
-//     const {authContext} = useAuth();
-//     const {cartContext, setItemCart} = useCartContext()
-//     const {getCart, cartHook} = useCart()
-//
-//     const [user, setUser] = useState({} as Auth);
-//
-//     getCart()
-//
-//     useEffect(() => {
-//         setTimeout(() => {
-//             if (cartHook.length !== cartContext.length) {
-//                 setItemCart(cartHook)
-//             }
-//         }, 1500)
-//     }, [cartHook])
-//
-//
-//     useEffect(() => {
-//         setUser(authContext)
-//     }, [authContext])
-//
-//     useEffect(() => {
-//         if (user.user && user.user.uid !== ADMIN_UID.UID) {
-//             Router.push("/main");
-//         }
-//     }, [user])
-//
-//
-//     return (
-//         <Box>
-//             Наші товари
-//             <Box>
-//                 Іграшки Ручної Роботи
-//                 <Image src={toy} alt={'toy'} width={200} height={200} onClick={()=>redirectTo('/catalog/toy')}/>
-//             </Box>
-//             <Box>
-//                 Жіночі вишиванки Ручної Роботи
-//                 <Image src={short_woman} alt={'short_woman'} width={200} height={200} onClick={()=>redirectTo('/catalog/embroidered_woman')}/>
-//             </Box>
-//             <Box>
-//                 Чоловічі вишиванки Ручної Роботи
-//                 <Image src={short_man} alt={'short_man'} width={200} height={200} onClick={()=>redirectTo('/catalog/embroidered_man')}/>
-//             </Box>
-//             <Box>
-//                 Намиста Ручної Роботи
-//                 <Image src={necklaces} alt={'necklaces'} width={200} height={200} onClick={()=>redirectTo('/catalog/necklaces')}/>
-//             </Box>
-//             <Box>
-//                 Сумки Ручної Роботи
-//                 <Image src={bag} alt={'bag'} width={200} height={200} onClick={()=>redirectTo('/catalog/bag')}/>
-//             </Box>
-//             <Box>
-//                 Лампи Ручної Роботи
-//                 <Image src={lamp} alt={'lamp'} width={200} height={200} onClick={()=>redirectTo('/catalog/lamp')}/>
-//             </Box>
-//         </Box>
-//     )
-// }
-//
-// export default MainPage
-
-
 import {useEffect, useState} from 'react';
 import {ADMIN_UID} from '@/services/localKey';
 import Router from 'next/router';
@@ -87,15 +6,13 @@ import {useAuth} from '@/hooks/useAuth';
 import {useCartContext} from '@/hooks/useCartContext';
 import {useCart} from '@/hooks/useCart';
 import {Box, Typography, Grid, Card, CardActionArea, CardContent, CardMedia} from '@mui/material';
-import Image from 'next/image';
-import toy from '../../assets/1.jpg';
-import short_woman from '../../assets/2.jpeg';
-import short_man from '../../assets/3.jpeg';
-import necklaces from '../../assets/4.jpeg';
-import bag from '../../assets/5.jpeg';
-import lamp from '../../assets/6.jpeg';
+import toy from '../../assets/toy.jpg';
+import short_woman from '../../assets/short_woman.jpeg';
+import short_man from '../../assets/short_man.jpeg';
+import necklaces from '../../assets/necklaces.jpeg';
+import bag from '../../assets/bag.jpeg';
+import lamp from '../../assets/lamp.jpeg';
 import {redirectTo} from '@/utils/redirect';
-
 const MainPage = () => {
     const {authContext} = useAuth();
     const {cartContext, setItemCart} = useCartContext();
