16499d900913a5f8819dd7a75d409982ff7e4440
Daloniil <62095873+Daloniil@users.noreply.github.com>
2023-05-08 01:03:59 +0300

    Bar

--- src/components/Bar/index.tsx
+++ src/components/Bar/index.tsx
@@ -58,26 +58,52 @@ export const Bar = ({setOpen, title}: BarProps) => {
                     aria-label="open drawer"
                     onClick={() => setOpen(true)}
                 >
-                    <MenuIcon/>
+                    <MenuIcon />
                 </IconButton>
                 <Typography
                     component="h1"
                     variant="h6"
                     color="inherit"
                     noWrap
-                    sx={{flexGrow: 1, padding: '0 0 0 5px'}}
+                    sx={{ flexGrow: 1, padding: '0 0 0 5px' }}
                 >
                     {title ? title : capitalizeFirstLetter(parsItemTitle)}
                 </Typography>
 
-                <ShoppingCartIcon onClick={() => redirectTo('/cart')} sx={{margin: '0 5px 0 0'}}/>
+                <ShoppingCartIcon onClick={() => redirectTo('/cart')} sx={{ margin: '0 5px 0 0' }} />
 
-                {user.user?.uid ? <Typography>{user.user.displayName}</Typography>
-                    : ''}
-                {user.user?.uid ? <Button onClick={signOutGoogle} color="inherit"
-                                          sx={{textTransform: "none", fontSize: '16px'}}>Вийти</Button> :
-                    <Button onClick={signIn} color="inherit"
-                            sx={{textTransform: "none", fontSize: '16px'}}>Авторизуватися</Button>}
+                {user.user?.uid ? (
+                    <Typography
+                        noWrap
+                        sx={{
+                            overflow: 'hidden',
+                            textOverflow: 'ellipsis',
+                            whiteSpace: 'nowrap',
+                            maxWidth: '150px',
+                        }}
+                    >
+                        {user.user.displayName}
+                    </Typography>
+                ) : (
+                    ''
+                )}
+                {user.user?.uid ? (
+                    <Button
+                        onClick={signOutGoogle}
+                        color="inherit"
+                        sx={{ textTransform: 'none', fontSize: '16px' }}
+                    >
+                        Вийти
+                    </Button>
+                ) : (
+                    <Button
+                        onClick={signIn}
+                        color="inherit"
+                        sx={{ textTransform: 'none', fontSize: '16px' }}
+                    >
+                        Авторизуватися
+                    </Button>
+                )}
             </Toolbar>
         </AppBar>
     );
